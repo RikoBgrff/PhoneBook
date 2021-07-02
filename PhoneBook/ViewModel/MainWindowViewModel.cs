8874c5c02035883bc2ee44ca913e0b3911992a08
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-07-02 13:00:28 +0400

    Add project files.

---
+++ PhoneBook/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.ViewModel
+{
+   public class MainWindowViewModel
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+        public MainWindowViewModel()
+        {
+
+        }
+        public List<Contact> ReturnContacts()
+        {
+            return Contacts;
+        }
+    }
+}
